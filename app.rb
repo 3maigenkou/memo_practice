cd47bc546fa2ba4aefe4915ff5be03e1fcf3d9ae
3maigenkou <kaipurio848@gmail.com>
2022-02-09 10:32:16 +0900

    保存先をDBに変更しました。

--- app.rb
+++ app.rb
@@ -2,31 +2,29 @@
 
 require 'sinatra'
 require 'sinatra/reloader'
-require 'securerandom'
-require 'json'
+require 'pg'
 
 helpers do
   include Rack::Utils
   alias_method :h, :escape_html
 end
 
+def connection
+  @conn = PG.connect(dbname: 'memotest')
+end
+
 get '/' do
-  sort_files = Dir.glob('data/*.json').sort_by { |file| File.birthtime(file) }
-  @memo_contents = sort_files.map do |file|
-    json_file = File.open(file, 'r')
-    JSON.load(json_file)
-  end
+  connection
+  @memo_contents = @conn.exec('SELECT * FROM memo')
   erb :index
 end
 
 post '/' do
-  id = SecureRandom.alphanumeric(10)
+  connection
   title = params[:title]
   comment = params[:comment]
-  File.open("data/#{id}.json", 'w') do |file|
-    json = { id: id.to_s, title: title.to_s, comment: comment.to_s }
-    JSON.dump(json, file)
-  end
+  new_memo = @conn.exec("INSERT INTO memo (title, comment) VALUES ('#{title}', '#{comment}') RETURNING id").first
+  id = new_memo['id']
   redirect to("/memo/#{id}")
 end
 
@@ -35,31 +33,29 @@ get '/new' do
 end
 
 get '/memo/:id' do |id|
-  @memo = File.open("data/#{id}.json", 'r') do |file|
-    JSON.load(file)
-  end
+  connection
+  @memo = @conn.exec("SELECT * FROM memo WHERE id = '#{id}'").first
   erb :show
 end
 
 get '/edit/:id' do |id|
-  @memo = File.open("data/#{id}.json", 'r') do |file|
-    JSON.load(file)
-  end
+  connection
+  @memo = @conn.exec("SELECT * FROM memo WHERE id = '#{id}'").first
   erb :edit
 end
 
 patch '/edit/:id' do |id|
   title = params[:title]
   comment = params[:comment]
-  File.open("data/#{params[:id]}.json", 'w') do |file|
-    json = { id: id.to_s, title: title.to_s, comment: comment.to_s }
-    JSON.dump(json, file)
-  end
+  connection
+  @conn.exec("UPDATE memo SET title = '#{title}', comment = '#{comment}' WHERE id ='#{id}'")
   redirect to("/memo/#{id}")
 end
 
 delete '/edit/:id' do
-  File.delete("data/#{params[:id]}.json")
+  id = params[:id]
+  connection
+  @conn.exec("DELETE FROM memo WHERE id ='#{id}'")
   redirect to('/')
 end
 
