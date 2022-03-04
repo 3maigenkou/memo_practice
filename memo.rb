615dbae7f970ce1ffa60673d5b37404cda619dbf
3maigenkou <kaipurio848@gmail.com>
2022-03-04 12:30:47 +0900

    SQLインジェクション対策をしました

--- memo.rb
+++ memo.rb
@@ -1,3 +1,5 @@
+# frozen_string_literal: true
+
 class Memo
   def initialize
     @conn = PG.connect(dbname: 'memotest')
@@ -8,18 +10,18 @@ class Memo
   end
 
   def create_new_memo(title, comment)
-    @conn.exec("INSERT INTO memo (title, comment) VALUES ('#{title}', '#{comment}') RETURNING id").first
+    @conn.exec('INSERT INTO memo (title, comment) VALUES ($1, $2) RETURNING id', [title, comment]).first
   end
 
   def get_memo_detail(id)
-    @conn.exec("SELECT * FROM memo WHERE id = '#{id}'").first
+    @conn.exec('SELECT * FROM memo WHERE id = $1', [id]).first
   end
 
   def update_memo(title, comment, id)
-    @conn.exec("UPDATE memo SET title = '#{title}', comment = '#{comment}' WHERE id ='#{id}'")
+    @conn.exec('UPDATE memo SET title = $1, comment = $2 WHERE id = $3', [title, comment, id])
   end
 
   def delete_memo(id)
-    @conn.exec("DELETE FROM memo WHERE id ='#{id}'")
+    @conn.exec('DELETE FROM memo WHERE id = $1', [id])
   end
 end
